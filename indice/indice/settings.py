bde7679f50949c19c00b1dc3279a9d3e5fd0b25c
jroca72 <jroca72@gmail.com>
2015-04-27 19:19:14 +0200

    cambios profundos

--- indice/indice/settings.py
+++ indice/indice/settings.py
@@ -82,3 +82,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, './templates').replace('\\', '/'),
+    )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = "/media/"
+
--- indice/indice/urls.py
+++ indice/indice/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'indice.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	 url(r'^principal/', include('principal.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^indice/', 'principal.views.indice', name = 'indice'),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
